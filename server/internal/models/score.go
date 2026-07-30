27c73e204587847cf567579fe1af79cdce6ce363
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-07-31 01:53:06 +0200

    Implemented Score table

---
+++ server/internal/models/score.go
@@ -0,0 +1,18 @@
+package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Score struct {
+	ID        uint   `gorm:"primaryKey"`
+	FilePath  string `gorm:"not null"`
+	OwnerID   uint   `gorm:"not null;uniqueIndex:idx_owner_name"`
+	Name      string `gorm:"not null;uniqueIndex:idx_owner_name"`
+	Composer  *string
+	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
