10e9b2372ccbe6bf8901a0b50542a52e002b21f6
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-07-31 02:21:10 +0200

    Impllemented Concert table

--- server/internal/models/concert.go
+++ server/internal/models/concert.go
@@ -1,5 +1,20 @@
 package models
 
-type Concert struct{}
+import (
+	"time"
 
-type ConcertItem struct{}
+	"gorm.io/gorm"
+)
+
+type Concert struct {
+	ID        uint           `gorm:"primaryKey"`
+	BandID    *uint          `gorm:"uniqueIndex:idx_band_name"`
+	UserID    *uint          `gorm:"uniqueIndex:idx_band_name"`
+	Name      string         `gorm:"not null;uniqueIndex:idx_band_name;uniqueIndex:idx_user_name"`
+	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	Band Band `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	User User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
