d3260ac5f581d89b654e18a8c031dc51c37efd82
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-07-31 02:15:05 +0200

    Implemented BandMember table

--- server/internal/models/band.go
+++ server/internal/models/band.go
@@ -17,7 +17,17 @@ type Band struct {
 	Manager User `gorm:"foreignKey:ManagerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
 }
 
-type BandMember struct{}
+type BandMember struct {
+	ID        uint           `gorm:"primaryKey"`
+	UserID    uint           `gorm:"not null;uniqueIndex:idx_user_band"`
+	BandID    uint           `gorm:"not null;uniqueIndex:idx_user_band"`
+	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	User User `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+	Band Band `gorm:"constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
 
 type SharedBandScore struct {
 	ID        uint           `gorm:"primaryKey"`
