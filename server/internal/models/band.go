7c0e5d30a599f279c267f4547c07ef9e166c2d1e
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-07-31 02:09:13 +0200

    Implemented Band table

--- server/internal/models/band.go
+++ server/internal/models/band.go
@@ -1,6 +1,21 @@
 package models
 
-type Band struct{}
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type Band struct {
+	ID        uint           `gorm:"primaryKey"`
+	ManagerID uint           `gorm:"not null;uniqueIndex:idx_band_name"`
+	Name      string         `gorm:"not null;uniqueIndex:idx_band_name"`
+	CreatedAt time.Time      `gorm:"not null;default:CURRENT_TIMESTAMP"`
+	UpdatedAt time.Time      `gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+
+	Manager User `gorm:"foreignKey:ManagerID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+}
 
 type BandMember struct{}
 
