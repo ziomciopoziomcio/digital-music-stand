66c4e7f5daf58e351750d8b7f562255b16088f9a
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-08-02 19:18:25 +0200

    Connect mock data to settings

--- client/main.go
+++ client/main.go
@@ -5,12 +5,20 @@ import (
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
 
+	"github.com/ziomciopoziomcio/digital-music-stand/client/system"
+	"github.com/ziomciopoziomcio/digital-music-stand/client/system/sysmock"
 	"github.com/ziomciopoziomcio/digital-music-stand/client/ui"
 )
 
 func main() {
 	myApp := app.New()
 	myWindow := myApp.NewWindow("Digital Music Stand")
+
+	//todo: use real drivers
+	netMgr := &sysmock.MockNetworkManager{Status: system.StatusDisconnected}
+	pwrMgr := &sysmock.MockPowerManager{BatteryLevel: 85, Charging: false}
+	medMgr := &sysmock.MockMediaManager{Volume: 50, Brightness: 80}
+	devMgr := &sysmock.MockDeviceManager{IsAwake: true}
 
 	mainWrapper := container.NewMax()
 
@@ -24,7 +32,7 @@ func main() {
 	}
 
 	showSettings = func() {
-		settingsView := ui.BuildSettings(myWindow, showDashboard)
+		settingsView := ui.BuildSettings(myWindow, showDashboard, netMgr, pwrMgr, medMgr, devMgr)
 		mainWrapper.Objects = []fyne.CanvasObject{settingsView}
 		mainWrapper.Refresh()
 	}
--- client/ui/settings.go
+++ client/ui/settings.go
@@ -1,14 +1,19 @@
 package ui
 
 import (
+	"fmt"
+
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
+
+	"github.com/ziomciopoziomcio/digital-music-stand/client/system"
 )
 
-func BuildSettings(w fyne.Window, onClose func()) *fyne.Container {
+func BuildSettings(w fyne.Window, onClose func(), netMgr system.NetworkManager, pwrMgr system.PowerManager, medMgr system.MediaManager, devMgr system.DeviceManager) *fyne.Container {
+	//todo: handle errors
 	contentWrapper := container.NewMax()
 
 	var showCategories func()
@@ -32,23 +37,37 @@ func BuildSettings(w fyne.Window, onClose func()) *fyne.Container {
 	buildNetworkView := func() fyne.CanvasObject {
 		scanBtn := widget.NewButtonWithIcon("Scan for Wi-Fi", theme.SearchIcon(), func() {})
 
-		wifiList := container.NewVBox(
-			widget.NewButton("Philharmonic_Guest", func() {}),
-			widget.NewButton("Concert_Hall_Stage", func() {}),
-			widget.NewButton("Free_WiFi", func() {}),
-		)
+		wifiList := container.NewVBox()
+		networks, _ := netMgr.GetAvailableNetworks()
+
+		for _, net := range networks {
+			ssid := net.SSID
+			wifiList.Add(widget.NewButton(fmt.Sprintf("%s (Signal: %d%%)", ssid, net.Strength), func() {
+				netMgr.ConnectWiFi(ssid, "mock_password")
+			}))
+		}
 
 		return container.NewBorder(container.NewPadded(scanBtn), nil, nil, nil, container.NewVScroll(wifiList))
 	}
 
 	buildMediaView := func() fyne.CanvasObject {
-		volLabel := widget.NewLabel("Volume")
+		currentVol, _ := medMgr.GetVolume()
+		volLabel := widget.NewLabel(fmt.Sprintf("Volume: %d%%", currentVol))
 		volSlider := widget.NewSlider(0, 100)
-		volSlider.SetValue(50)
+		volSlider.SetValue(float64(currentVol))
+		volSlider.OnChanged = func(val float64) {
+			medMgr.SetVolume(int(val))
+			volLabel.SetText(fmt.Sprintf("Volume: %d%%", int(val)))
+		}
 
-		brightLabel := widget.NewLabel("Screen Brightness")
+		currentBright, _ := medMgr.GetBrightness()
+		brightLabel := widget.NewLabel(fmt.Sprintf("Screen Brightness: %d%%", currentBright))
 		brightSlider := widget.NewSlider(10, 100)
-		brightSlider.SetValue(80)
+		brightSlider.SetValue(float64(currentBright))
+		brightSlider.OnChanged = func(val float64) {
+			medMgr.SetBrightness(int(val))
+			brightLabel.SetText(fmt.Sprintf("Screen Brightness: %d%%", int(val)))
+		}
 
 		return container.NewVBox(
 			volLabel,
@@ -60,9 +79,17 @@ func BuildSettings(w fyne.Window, onClose func()) *fyne.Container {
 	}
 
 	buildPowerView := func() fyne.CanvasObject {
-		battLabel := widget.NewLabel("Battery Level: 85% (Charging)")
+		battLevel, _ := pwrMgr.GetBatteryPercentage()
+		charging, _ := pwrMgr.IsCharging()
+
+		status := "Discharging"
+		if charging {
+			status = "Charging"
+		}
+
+		battLabel := widget.NewLabel(fmt.Sprintf("Battery Level: %d%% (%s)", battLevel, status))
 		battBar := widget.NewProgressBar()
-		battBar.SetValue(0.85)
+		battBar.SetValue(float64(battLevel) / 100.0)
 
 		return container.NewVBox(
 			battLabel,
@@ -71,11 +98,17 @@ func BuildSettings(w fyne.Window, onClose func()) *fyne.Container {
 	}
 
 	buildSystemView := func() fyne.CanvasObject {
-		awakeCheck := widget.NewCheck("Keep Screen Awake", func(checked bool) {})
+		awakeCheck := widget.NewCheck("Keep Screen Awake", func(checked bool) {
+			devMgr.SetKeepAwake(checked)
+		})
 		awakeCheck.SetChecked(true)
 
-		rebootBtn := widget.NewButtonWithIcon("Reboot Device", theme.ViewRefreshIcon(), func() {})
-		shutdownBtn := widget.NewButtonWithIcon("Shutdown Device", theme.CancelIcon(), func() {})
+		rebootBtn := widget.NewButtonWithIcon("Reboot Device", theme.ViewRefreshIcon(), func() {
+			devMgr.Reboot()
+		})
+		shutdownBtn := widget.NewButtonWithIcon("Shutdown Device", theme.CancelIcon(), func() {
+			devMgr.Shutdown()
+		})
 		shutdownBtn.Importance = widget.DangerImportance
 
 		return container.NewVBox(
