cdccbb41456e63f2cf5fa5de12e41c100dca0952
Jędrzej Niedzielski <jedrzejnie@gmail.com>
2026-08-21 18:38:23 +0200

    Implement AutoKeyboardEntry

---
+++ client/ui/keyboard.go
@@ -0,0 +1,65 @@
+package ui
+
+import (
+	"os/exec"
+
+	"fyne.io/fyne/v2/widget"
+)
+
+var keyboardCmd *exec.Cmd
+
+func ShowKeyboard() {
+	if keyboardCmd != nil && keyboardCmd.Process != nil {
+		return
+	}
+	keyboardCmd = exec.Command("matchbox-keyboard")
+	_ = keyboardCmd.Start()
+}
+
+func HideKeyboard() {
+	if keyboardCmd != nil && keyboardCmd.Process != nil {
+		_ = keyboardCmd.Process.Kill()
+		keyboardCmd = nil
+	}
+}
+
+type AutoKeyboardEntry struct {
+	widget.Entry
+}
+
+func NewAutoKeyboardEntry() *AutoKeyboardEntry {
+	e := &AutoKeyboardEntry{}
+	e.ExtendBaseWidget(e)
+	return e
+}
+
+func (e *AutoKeyboardEntry) FocusGained() {
+	e.Entry.FocusGained()
+	ShowKeyboard()
+}
+
+func (e *AutoKeyboardEntry) FocusLost() {
+	e.Entry.FocusLost()
+	HideKeyboard()
+}
+
+type AutoKeyboardPasswordEntry struct {
+	widget.Entry
+}
+
+func NewAutoKeyboardPasswordEntry() *AutoKeyboardPasswordEntry {
+	e := &AutoKeyboardPasswordEntry{}
+	e.Password = true
+	e.ExtendBaseWidget(e)
+	return e
+}
+
+func (e *AutoKeyboardPasswordEntry) FocusGained() {
+	e.Entry.FocusGained()
+	ShowKeyboard()
+}
+
+func (e *AutoKeyboardPasswordEntry) FocusLost() {
+	e.Entry.FocusLost()
+	HideKeyboard()
+}
